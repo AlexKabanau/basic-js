fe84730d0d3aef23965366480ec3d593fac5ab3f
Alexandr Kabanau <Azraill1989@gmail.com>
2022-10-15 16:59:00 +0300

    feat: transform-array and hanoi-tower - done

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,8 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
+function calculateHanoi(disksNumber, turnsSpeed) {
+  let answer = {};
+  answer.turns;
+  answer.seconds;
+
+  answer.turns = (2**disksNumber) - 1;
+  answer.seconds = Math.floor(answer.turns * 3600 / turnsSpeed);
+  return answer
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,8 +13,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (Array.isArray(arr)) {
+    let answer = [];
+    for (let i=0; i<arr.length; i++) {
+      if (arr[i] == `--discard-next`) {
+        i++;
+      } else if (arr[i] == `--discard-prev`) {
+        if (arr[i-2] != `--discard-next`) {
+          answer.pop();
+        } else {
+          continue
+        }
+        
+      } else if (arr[i] == `--double-next`) {
+        if (arr[i+1]) {
+          answer.push(arr[i+1]);
+        } else {
+          continue
+        }
+      } else if (arr[i] == `--double-prev`) {
+        if (arr[i-1] && arr[i-2] != `--discard-next`) {
+          answer.push(arr[i-1]);
+        } else {
+          continue
+        }
+      } else {
+        answer.push(arr[i]);
+      }
+    }
+    return answer;
+  } else {
+    throw new Error ("'arr' parameter must be an instance of the Array!");
+  }
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
