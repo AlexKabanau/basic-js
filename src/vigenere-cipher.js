b5be43d23d96ba5549d77ef277c767dc3c01523d
Alexandr Kabanau <Azraill1989@gmail.com>
2022-10-16 18:20:01 +0300

    feat: vigenere - done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,85 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
+  constructor(reverse) {
+    this.reverse = false;
+    if (reverse == false) {
+      this.reverse = true;
+    };
+  }
+  encrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let answer = '';
+    let keyI = 0;
+
+    for (let i=0; i<str.length; i++) {
+      let currentLetter = str[i];
+      if ((currentLetter.charCodeAt()<=90) && (currentLetter.charCodeAt()>=65)) {
+        let indexStr = currentLetter.charCodeAt()-65;
+        let indexKey = key[keyI%key.length].toUpperCase().charCodeAt()-65;
+        answer = answer + String.fromCharCode((indexStr+indexKey)%26 + 65);
+        keyI++;
+      } else if (((str[i].charCodeAt()>=97) && (str[i].charCodeAt()<=122))) {
+        let indexStr = currentLetter.charCodeAt()-97;
+        let indexKey = key[keyI%key.length].charCodeAt()-97;
+        answer = answer + String.fromCharCode((indexStr+indexKey)%26 + 97);
+        keyI++;
+      } else {
+        answer = answer + currentLetter;
+      }
+    }
+    if (this.reverse == false) {
+      return answer
+          .toUpperCase();
+    } else {
+      return answer
+          .split('')
+          .reverse()
+          .join('')
+          .toUpperCase();
+    }
+    
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  decrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let answer = '';
+    let keyI = 0;
+
+    for (let i=0; i<str.length; i++) {
+      let currentLetter = str[i];
+      if ((currentLetter.charCodeAt()<=90) && (currentLetter.charCodeAt()>=65)) {
+        let indexStr = currentLetter.charCodeAt()-65;
+        let indexKey = key[keyI%key.length].toUpperCase().charCodeAt()-65;
+        answer = answer + String.fromCharCode((((indexStr-indexKey)%26)+26)%26 + 65);
+        keyI++;
+      } else if (((str[i].charCodeAt()>=97) && (str[i].charCodeAt()<=122))) {
+        let indexStr = currentLetter.charCodeAt()-97;
+        let indexKey = key[keyI%key.length].toUpperCase().charCodeAt()-97;
+        answer = answer + String.fromCharCode((indexStr-indexKey)%26 + 97);
+        keyI++;
+      } else {
+        answer = answer + currentLetter;
+      }
+    }
+    if (this.reverse == false) {
+      return answer
+          .toUpperCase();
+    } else {
+      return answer
+          .split('')
+          .reverse()
+          .join('')
+          .toUpperCase();
+    }
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
